f2b9a2a1ceadc4609fac4180259baa55df831a5e
mdabutalebdev <mdabutaleb.dev@gmail.com>
2024-10-11 23:28:06 +0600

    innovate

---
+++ js/tailwind.config.js
@@ -0,0 +1,50 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      // Color part start
+      colors: {
+        headerBg: "rgba(22, 12, 109, 0.5)",
+        hoverColor:"#1BBF00",
+        HeaderTextColor:"#EFF4FA",
+        BannarOverlayColor:"rgba(106, 98, 197, 0.9);",
+        TextPcolor:"#726E9E",
+        Texth3color:"#141135",
+        Texth4color:"#030314",
+        bc:"#4F585F",
+        btntext:"#F9F9F9",
+        cardbg:"#F6F5FF",
+        cardoverlaycolor:"#6B62C5",
+        cardbefore:"rgba(25, 191, 0, 0.3)"
+       
+      },
+      // Color part end
+      // font family start
+      fontFamily: {
+        OpenSans:"'Open Sans', sans-serif;",
+        Paprika:"'Paprika', cursive;",
+        Nunito:"'Nunito Sans', sans-serif;",
+      },
+      // font family send
+
+      // container part start
+      width: {
+        headercontainer: "1170px",
+        contactContainer:"1147px",
+        impactContainer:"1079px",
+         
+      },
+      // container part end
+
+      // background images start
+      backgroundImage: {
+        bannerbg:"url('images/bannar images.jpg')",
+        counterimages:"url('images/bg_two.png')",
+        cotactbg:"url('images/bg_three.png')",
+        impectbg:"url('images/impectBg.png')",
+         
+      },
+      // background images end
+      
+    },
+  },
+}
